4f17fe1065d73c5cf01c0482f045e5ad41022a2e
Kuldeep Tiwari <tiwarikuldeep307@gmail.com>
2024-08-22 10:41:39 +0530

    comparsion lecture in javascript

---
+++ comparion.js
@@ -0,0 +1,31 @@
+/*console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+*/
+
+//console.log("2"> 1);
+//console.log("02"> 1);
+
+
+console.log("2" > 1);  // Expected output: true
+console.log("02" > 1); // Expected output: true
+
+
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+// avoid this type of conversion
+
+
+
+
+
+console.log("2"===2);
+
+
---
+++ datatypeconversion.js
@@ -0,0 +1,48 @@
+
+
+//*************OPerations******** ****//
+
+
+let value = 3
+let negValue = -value
+// console.log(negvalue)
+
+// console.log(2+2)
+// similarly other arthmatic operations are possible
+
+
+let str1 = "hello"
+let str2 = "kuldeep"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" +2+ 2);
+console.log(1 +2+ "2");
+
+console.log(true);
+
+console.log(+true);
+
+
+
+let num1,num2,num3
+
+//num1 = num2 = num3 = 2+2 aaisa nhi krna hai readability bekar ho jata hai
+
+let gamecounter = 100
+gamecounter++ ;
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
+
+
